867ed0f01592f50f040e4250299606b139a563fc
Rajeev964 <rajeevrepswal9351@gmail.com>
2024-06-18 19:01:24 +0000

    Datatype Conversion

---
+++ 01_basics/dataconvers.js
@@ -0,0 +1,25 @@
+let score="true"
+
+console.log(typeof score);
+
+console.log(typeof (score) );
+
+
+let value=Number(score)
+
+console.log(typeof value);
+
+console.log(value);
+
+let isLogIn=1
+
+let BooleanLogIn=Boolean(isLogIn);
+
+console.log(typeof isLogIn);
+
+console.log(typeof BooleanLogIn);
+
+console.log(BooleanLogIn);
+
+
+
