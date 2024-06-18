45d11c8415f8bb7bec66de4401a98253c676cd1d
Rajeev964 <rajeevrepswal9351@gmail.com>
2024-06-18 16:16:04 +0000

    Done

---
+++ 01_basics/variable.js
@@ -0,0 +1,21 @@
+const accountId=1444
+
+let accountEmail="rajeevrepw9413@gmail.com"
+
+var accountPsd="12345"
+
+accountCity="Jaipur"
+
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPsd,accountCity]);
+
+accountEmail="rajeevrepw9351@gmail.com"
+
+accountPsd="1234567889"
+accountCity="Sikar"
+
+console.table([accountId,accountEmail,accountPsd,accountCity]);
+
