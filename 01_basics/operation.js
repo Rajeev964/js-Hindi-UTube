a7c373486fa87eaac3f3c7e40797094ea6a18939
Rajeev964 <rajeevrepswal9351@gmail.com>
2024-06-20 05:36:22 +0000

    Operation

---
+++ 01_basics/operation.js
@@ -0,0 +1,33 @@
+let str1="Hello"
+
+let str2="Raju"
+
+let str3=str1+str2
+
+console.log(str3);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2%3);
+
+console.log(2+2*3);
+
+console.log("1"+2);
+
+console.log(1+"2");
+
+console.log("1"+2+2);
+
+console.log(1+2+"2")
+
+console.log(true);
+
+console.log(+true);
+
+console.log(+"1");
+
+let count=100;
+count++;
+
+console.log(count);
