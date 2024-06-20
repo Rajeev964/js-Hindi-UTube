9977412addddbb70a8078f3919b5421f84577a1f
Rajeev964 <rajeevrepswal9351@gmail.com>
2024-06-20 08:36:29 +0000

    Done

---
+++ 01_basics/stack.js
@@ -0,0 +1,18 @@
+let myYou="Repswal"
+
+let another=myYou
+
+another="trip"
+console.log(myYou);
+console.log(another);
+
+let user1={
+    email:"raju@gmail.com",
+    upi:"user@ybl",
+}
+
+let user2=user1
+user2.email="repswal@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email)
