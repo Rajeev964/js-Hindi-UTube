3171cd8ded971d3d55f08cf34b0759d2f2093d98
Rajeev964 <rajeevrepswal9351@gmail.com>
2024-06-21 04:50:43 +0000

    Done

---
+++ 01_basics/number.js
@@ -0,0 +1,41 @@
+const score=400
+const balance = new Number(100)
+
+console.log(balance);
+
+console.log(score);
+
+console.log(balance.toFixed(3));
+
+const raju=123.8966
+
+console.log(raju.toPrecision(5));
+
+const me=10000000
+console.log(me.toLocaleString('en-IN'));
+
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6));
+
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.6));
+console.log(Math.min(4,3,6,8));
+console.log(Math.random());
+
+
+console.log(Math.floor(Math.random()*10+1));
+
+const min=1;
+
+const max=6;
+console.log(Math.floor((Math.random()*(max-min+1))+min));
+
+
+
+
+
+
